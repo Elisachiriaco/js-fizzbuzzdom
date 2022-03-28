890fba608c6720f395610f9d022d4004753a95d6
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-03-28 19:53:04 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let numero = 100;
+const app = document.getElementById('app');
+const row = document.createElement('div');
+row.setAttribute('class', 'row g-2')
+app.append(row);
+
+for (let i = 1; i <= numero; i++) {
+
+    const col = document.createElement('div');
+    col.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-2');
+    const box = document.createElement('div');
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log((i) + " : FizzBuzz");
+        box.innerHTML += `<h2>FizzBuzz</h2>`
+        box.setAttribute('class', 'square');
+        for (let c = 0; c < i.length; c++) {
+        }
+
+    } else if (i % 5 === 0) {
+        console.log((i) + " : Buzz");
+        box.innerHTML += `<h2>Buzz</h2>`
+        box.setAttribute('class', 'square');
+        for (let c = 0; c < i.length; c++) {
+        }
+
+    } else if (i % 3 === 0) {
+        console.log((i) + " : Fizz");
+        box.innerHTML += `<h2>Fizz</h2>`
+        box.setAttribute('class', 'square');
+        for (let c = 0; c < i.length; c++) {
+        }
+
+    } else {
+        box.innerHTML += `<h2>${[i]}</h2>`
+        box.setAttribute('class', 'square');
+    }
+
+    col.append(box);
+    row.append(col);
+    app.append(row);
+}
